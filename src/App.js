653dc7a58bd33006f62842cd111d9cf7bbc45b1b
panneer <panneerselvam@10decoders.in>
2023-08-24 13:10:04 +0530

    own filtration task

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [inputNumbers, setInputNumbers] = useState("");
+  const [filteredNumbers, setFilteredNumbers] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputNumbers(event.target.value);
+  };
+
+  const ownFilter = (number, callBack) => {
+    const filteredNum = [];
+    for (const item of number) {
+      if (callBack(item)) {
+        filteredNum.push(item);
+      }
+    }
+    return filteredNum;
+  };
+
+  const evenNum = (number) => number % 2 === 0;
+
+  const filterNumbers = () => {
+    const numbersArray = inputNumbers.split(",").map(Number);
+    // console.log(numbersArray);
+    const evenNumbers = ownFilter(numbersArray, evenNum);
+    setFilteredNumbers(evenNumbers);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Number Filter App</h1>
+        <input
+          type="text"
+          placeholder="Enter numbers separated by commas"
+          value={inputNumbers}
+          onChange={handleInputChange}
+        />
+        <button onClick={filterNumbers}>Filter Even Numbers</button>
+        {filteredNumbers.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th>Even Numbers</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredNumbers.map((number, index) => (
+                <tr key={index}>
+                  <td>{number}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </header>
     </div>
   );
